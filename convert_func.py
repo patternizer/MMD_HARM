335e73dda6dedc68f5b8ef48a8de6ae9ef4bbfa4
Michael Taylor <patternizer@gmail.com>
2019-06-10 15:38:29 +0100

    Add WV term to measurement equations + derivative dependencies

--- convert_func.py
+++ convert_func.py
@@ -10,45 +10,48 @@ def dbyd(a1,a2,b1,b2):
     dbyd = ((a2-a1)/(b2-b1))
     return dbyd
 
-#############################################################
-#                                                           #
-#  FUNCTIONS FOR CONVERTING BETWEEN BT, COUNTS AND RADIANCE #
-#                                                           #
-#  read_in_LUT()                                            #
-#  REQUIRES avhrr_sat to be in the format:                  #
-#  N12,N14,N15,N16,N17,N18,N19,MTA,MTB                      #
-#  RETURNS LUT dictionary with groups "L" and "BT"          #
-#                                                           #
-#  rad2bt()                                                 #
-#  REQUIRES array of radiance, channel (either 37, 11 or 12)#
-#  and lut outputfrom read_in_LUT (sensor specific)         #
-#  RETURNS BT in array of same initial shape of L           #
-#                                                           #
-#  bt2rad()                                                 #
-#  REQUIRES array of BT, channel (either 37, 11 or 12)      #
-#  and lut output from read_in_LUT (sensor specific)        #
-#  RETURNS L in array of same initial shape of BT           #
-#                                                           #
-#  dbt_drad()                                               #
-#  REQUIRES array of radiance, channel (either 37, 11 or 12)#
-#  and lut output from read_in_LUT (sensor specific)        #
-#  RETURNS dbt_drad array in the same shape as L input      #
-#                                                           #
-#  drad_da()                                                #
-#  REQUIRES Radience (L_ict), arrays C_e, C_s, C_ict,       #
-#  T_ict, T_inst, T_mean and T_sdev. Also requires channel  #
-#  and satellite sensor.                                    #
-#  RETURNS: If 3.7um channel, then two derivatives of       #
-#  radiance with respect to a1 and a2 coefficents. Else     #
-#  it returns three derivitives of radiance with respect    #
-#  to a1, a2 and a3 coefficents                             #
-#                                                           #
-#  count2rad()                                              #
-#  REQUIRES arrays Ce,Cs,Cict,Lict,Tinst, channel (either   #
-#  37, 11 or 12), and harmon coefficients a0,a1,a2,a3       #
-#  RETURNS radiance in array with same shape as L input     #
-#                                                           #
-#############################################################
+##############################################################
+#                                                            #
+#  FUNCTIONS FOR CONVERTING BETWEEN BT, COUNTS AND RADIANCE  #
+#                                                            #  
+#  NB: channel=3 --> 3.7 micron                              #
+#  NB: channel=4 -->  11 micron                              #
+#  NB: channel=5 -->  12 micron                              #
+#                                                            #  
+#  read_in_LUT()                                             #
+#  REQUIRES avhrr_sat to be in the format:                   #
+#  N12,N14,N15,N16,N17,N18,N19,MTA,MTB                       #
+#  RETURNS LUT dictionary with groups "L" and "BT"           #
+#                                                            #
+#  rad2bt()                                                  #
+#  REQUIRES array of radiance, channel (either 3, 4 or 5)    #
+#  and lut output from read_in_LUT (sensor specific)          #
+#  RETURNS BT in array of same initial shape of L            # 
+#                                                            # 
+#  bt2rad()                                                  #
+#  REQUIRES array of BT, channel (either 3, 4 or 5)          #
+#  and lut output from read_in_LUT (sensor specific)         #
+#  RETURNS L in array of same initial shape of BT            #
+#                                                            #
+#  dbt_drad()                                                #
+#  REQUIRES array of radiance, channel (either 3, 4 or 5)    #
+#  and lut output from read_in_LUT (sensor specific)         #
+#  RETURNS dbt_drad array in the same shape as L input       #
+#                                                            #
+#  drad_da()                                                 #
+#  REQUIRES arrays of Lict,Ce,Cs,Cict,Tict,Tinst,WV          #
+#  Also requires channel and satellite sensor.               #
+#  RETURNS: If 3.7um channel, then three derivatives of      #
+#  radiance with respect to a2,a4,a5 coefficents. Else       #
+#  it returns four derivitives of radiance with respect      #
+#  to a2,a3,a4 and a5 coefficents                            #
+#                                                            #
+#  count2rad()                                               #
+#  REQUIRES arrays Ce,Cs,Cict,Lict,Tinst,WV,channel (either  #
+#  3, 4 or 5), and harmonisation coeffs a1,a2,a3,a4,a5       #
+#  RETURNS radiance in array with same shape as Le input     #
+#                                                            #
+##############################################################
 
 def read_in_LUT(avhrr_sat):
     LUT = {}
@@ -83,30 +86,40 @@ def dbt_drad(L,channel,lut):
                 dbtdrad[i,j] = (dbyd(lut['BT'][idx,channel],lut['BT'][idx+1,channel],lut['L'][idx,channel],lut['L'][idx+1,channel]))
     return dbtdrad
 
-def drad_da(L_ict,C_e,C_s,C_ict,T_ict,T_inst,T_mean,T_sdev,channel,avhrr_sat):
+def drad_da(Lict,Ce,Cs,Cict,Tict,Tinst,WV,channel,avhrr_sat):
+    '''
+    Added derivative for WV term
+    '''
     try:
         if channel == 3:
-            drad_da1 = L_ict/(C_ict - C_s)
-            drad_da2 = (T_inst - T_mean) / T_sdev
-            return drad_da1,drad_da2
+            drad_da2 = Lict/(Cict - Cs)
+            drad_da3 = Tinst
+            drad_da4 = WV
+            return drad_da2,drad_da3,drad_da4
         elif channel > 3:
-            drad_da1 = L_ict/(C_ict - C_s)
-            drad_da2 = (C_e - C_ict) * (C_e - C_s)
-            drad_da3 = (T_inst - T_mean) / T_sdev
-            return drad_da1,drad_da2,drad_da3
+            drad_da2 = Lict/(Cict - Cs)
+            drad_da3 = (Ce - Cict) * (Ce - Cs)
+            drad_da4 = Tinst
+            drad_da5 = WV
+            return drad_da2,drad_da3,drad_da4,drad_da5
     except:
         print("No FIDUCEO thermal channel selected: channel=", channel, " < 3")
     
-def count2rad(Ce,Cs,Cict,Lict,Tinst,channel,a0,a1,a2,a3):
+def count2rad(Ce,Cs,Cict,Lict,Tinst,WV,channel,a1,a2,a3,a4,a5):
+    '''
+    NB: Tinst is the normalized temperature (T - T_mean)/T_std
+    NB: Additional WV term added in the measurement equations
+    '''
     L = np.empty(shape=(Ce.shape[0],Ce.shape[1]))
     try:
         if channel == 3:
-            L = a0 + ((Lict * (0.985140 + a1)) / (Cict - Cs)) * (Ce - Cs) + a2 * Tinst
+            L = a1 + ((Lict * (0.985140 + a2)) / (Cict - Cs)) * (Ce - Cs) + a3 * Tinst + a4 * WV
         elif channel > 3:  
-            L = a0 + ((Lict * (0.985140 + a1)) / (Cict - Cs) + a2 * (Ce - Cict)) * (Ce - Cs) + a3 * Tinst
+            L = a1 + ((Lict * (0.985140 + a2)) / (Cict - Cs) + a3 * (Ce - Cict)) * (Ce - Cs) + a4 * Tinst + a5 * WV
     except:
         print("No FIDUCEO thermal channel selected: channel=", channel, " < 3")
 
     return L
+
 
 
